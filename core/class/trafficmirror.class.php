bc355efd6ccda71bd46b124466c2b4a4fac71ecc
Benoit <benoit5672@yahoo.fr>
2021-10-28 19:37:59 +0200

    remove .htaccess in core/php that prevent the callback  function to be called

--- core/class/trafficmirror.class.php
+++ core/class/trafficmirror.class.php
@@ -288,8 +288,7 @@ class trafficmirror extends eqLogic {
               if (is_object($mirror) && $mirror->getIsEnable() == 1) {
 				  $stats = $mirror->daemonGetStatistics();
 				  if ($stats === undefined) {
-					  log::add('trafficmirror', 'error',
-					           __('Le miroir ' . $mirror->getId() . 'n\'est pas présent dans le démon', __FILE__));
+					  log::add('trafficmirror', 'error', __('Le miroir ' . $mirror->getId() . 'n\'est pas présent dans le démon', __FILE__));
 					  continue;
 				  }
 			 	  $mirror->updateStatistics($stats);
