56490cdc6f64518b1f73e51f01a597b238a0b24f
Benoit <benoit5672@yahoo.fr>
2021-12-03 14:35:23 +0100

    Update install.php to delete legacy nodejs files from previous install.

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -22,11 +22,11 @@ function deleteDirectory($dirPath) {
     if (is_dir($dirPath)) {
         $objects = scandir($dirPath);
         foreach ($objects as $object) {
-            if ($object != "." && $object !="..") {
-                if (filetype($dirPath . DIRECTORY_SEPARATOR . $object) == "dir") {
-                    deleteDirectory($dirPath . DIRECTORY_SEPARATOR . $object);
+            if ($object != "." && $object != "..") {
+                if (filetype($dirPath . '/' . $object) == "dir") {
+                    deleteDirectory($dirPath . '/' . $object);
                 } else {
-                    unlink($dirPath . DIRECTORY_SEPARATOR . $object);
+                    unlink($dirPath . '/' . $object);
                 }
             }
         }
@@ -35,27 +35,32 @@ function deleteDirectory($dirPath) {
     }
 }
 
+function removeNodeJSInstall() {
+    // files to remove, after migrating from nodejs to python3
+    $files = ['dependance.lib', 'install_nodejs.sh', 'install.sh',
+              'package.json', 'package-lock.json',
+              'tcp-mirror.js', 'udp-mirror.js',
+              'trafficmirrord.js', 'trafficmirror_version',
+              'util_log.js'];
+    $dirs =  ['node_modules'];
+    foreach ($files as $f) {
+        $path = dirname(__FILE__) . '/../resources/' . $f;
+        unlink($path);
+    }
+    foreach ($dirs as $d) {
+        $path = dirname(__FILE__) . '/../resources/' . $d;
+        deleteDirectory($path);
+    }
+}
+
 // Fonction exécutée automatiquement après l'installation du plugin
   function trafficmirror_install() {
-
+      removeNodeJSInstall();
   }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
   function trafficmirror_update() {
-      // files to remove, after migrating from nodejs to python3
-      $files = ['dependance.lib', 'install_nodejs.sh', 'install.sh',
-                'package.json', 'tcp-mirror.js', 'udp-mirror.js',
-                'trafficmirrord.js', 'trafficmirror_version',
-                'util_log.js'];
-      $dirs =  ['node_modules'];
-      foreach ($files as $f) {
-          $path = dirname(__FILE__) . '/' . $f;
-          unlink($path);
-      }
-      foreach ($dirs as $d) {
-          $path = dirname(__FILE__) . '/' . $d;
-          deleteDirectory($path);
-      }
+      removeNodeJSInstall();
   }
 
 // Fonction exécutée automatiquement après la suppression du plugin
